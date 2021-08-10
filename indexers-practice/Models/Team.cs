7ab30bbdfd1424c648e9c1996bf81699d905ed8c
slowiakjakub <dwabas9@gmail.com>
2021-08-10 17:39:34 +0200

    initial commit

---
+++ indexers-practice/Models/Team.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersPractice
+{
+    public class Team
+    {
+        public List<Person> TeamMembers { get; set; } = new List<Person>();
+
+        public Team() // Generate some dummy data
+        {
+            TeamMembers.Add(new Person("Josh", 22));
+            TeamMembers.Add(new Person("Anne", 21));
+            TeamMembers.Add(new Person("James", 19));
+            TeamMembers.Add(new Person("Evelyn", 19));
+            TeamMembers.Add(new Person("Harper", 24));
+            TeamMembers.Add(new Person("Mason", 24));
+            TeamMembers.Add(new Person("Ella", 26));
+            TeamMembers.Add(new Person("Jackson", 17));
+            TeamMembers.Add(new Person("Avery", 21));
+            TeamMembers.Add(new Person("Scarlett", 22));
+        }
+        public Person this[string name]
+        {
+            get
+            {
+                return TeamMembers.Find(x => x.Name == name);
+            }
+        }
+
+    }
+}
---
+++ indexers-practice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a person's name to search for in a team: ");
+            string name = Console.ReadLine();
+
+            Team team = new Team();
+            Person person = team[name]; // making use of indexers
+
+            Console.Clear();
+            if (person != null)
+            {
+                Console.WriteLine($"Teammate name: {person.Name}\n" +
+                $"Teammate age: {person.Age} ");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Cannot find a person named: {name}");
+            }
+
+        }
+    }
+}
